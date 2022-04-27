8fea385149d36b1e29bec3a2c38044d41e040e94
Amir Razmjou <arazmjou@cloudflare.com>
2022-04-27 00:03:22 -0500

    Initial commit

---
+++ src/main/java/com/cloudflare/Main.java
@@ -0,0 +1,9 @@
+package com.cloudflare;
+
+public class Main {
+    public static void main(String[] args) {
+        EPServiceProvider engine = EPServiceProviderManager.getDefaultProvider();
+
+        System.out.println("Hello world!");
+    }
+}
